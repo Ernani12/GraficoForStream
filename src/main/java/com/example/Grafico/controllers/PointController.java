cedbeac78bcfdc39f28c3e476fca50a7d370269e
Ernani12 <39600025+Ernani12@users.noreply.github.com>
2023-07-17 15:34:00 -0300

    API  consumed by react ok

--- src/main/java/com/example/Grafico/controllers/PointController.java
+++ src/main/java/com/example/Grafico/controllers/PointController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,7 @@ import com.example.Grafico.repositorio.PointRepositorio;
 
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("Point")
 public class PointController {
 
